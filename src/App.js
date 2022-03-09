a51278eaa7b95f078031c1be8bac77206f8aebca
kene17 <keneojukwu17@gmail.com>
2022-03-09 00:37:43 -0500

    completed user authentification

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import Login from "./pages/Login";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import {signOut} from "firebase/auth";
+import { auth } from "./firebase-config";
+function App() {
+  const [isAuth, setIsAuth] = useState(false);
+  //when user is signout out, local storage is cleared and you're redirected to login page
+  const signUserOut = () =>{
+    signOut(auth).then(() => {
+      localStorage.clear()
+      setIsAuth(false);
+      //can't use useNavigate when you've routers
+      window.location.pathname = "/login";
+    });
+  }
+  return (
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/createpost">Create Post</Link>
+        {!isAuth ? <Link to="/login">Login</Link> : <button onClick={signUserOut}>Logout</button>}
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/createpost" element={<CreatePost />} />
+        <Route path="/login" element={<Login setIsAuth={setIsAuth} />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/firebase-config.js
@@ -0,0 +1,25 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAd5G_8ctWgNypGSuz3jyyKjqy0ZTtdwUs",
+  authDomain: "blogproject-b8145.firebaseapp.com",
+  projectId: "blogproject-b8145",
+  storageBucket: "blogproject-b8145.appspot.com",
+  messagingSenderId: "710454205059",
+  appId: "1:710454205059:web:5ca470aa79c1e87843d9b6",
+  measurementId: "G-RHJ7QM38MT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider(); //	The authentication method used ("password", "anonymous", "facebook", "github", "google", or "twitter").
+
+
